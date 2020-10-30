5cc2defb7a9d3a5820cd4ae38cea341d0f3ad62b
Huqingnan123 <897703334@qq.com>
2020-10-30 13:12:23 +0800

    2020-10-30

--- 0007.整数反转/0007-整数反转.cpp
+++ 0007.整数反转/0007-整数反转.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     int reverse(int x) {
-        vector<char> vec;
-        int i,status=1;
-        int number=0;
+        vector<int> vec;
+        int status=1;
         if(x<0)
             status=0;
-        x=abs(x);
-        do
+        x=abs(x);            //将x转化为正数去反转，status标记正负
+        while(x != 0)
         {
-            i=x%10;
-            x=x/10;
-            vec.push_back(i+'0');
-        }while(x!=0);
-        if(status==1)
-        {
-            for(i=0;i<vec.size();i++)
-            {   
-                if(number>(pow(2,31)-1)/10||number<(-pow(2,31))/10)
-                    return 0;
-                number=number*10+(vec[i]-'0');
-            }
+            vec.push_back(x%10);
+            x /= 10;
+        } 
+        int number=0;        //number储存和
+        for(int i=0; i<vec.size(); i++)
+        {   
+            if(number>(pow(2.0,31)-1)/10 || number<(-pow(2.0,31))/10)
+                return 0;
+            number=number*10 + vec[i];
         }
-        else
-        {
-            for(i=0;i<vec.size();i++)
-            {   
-                if(number>(pow(2,31)-1)/10||number<(-pow(2,31))/10)
-                    return 0;
-                number=number*10-(vec[i]-'0');
-            }
-        }
-        return number; 
+        return (status ? number : -number);
     }
 };
---
+++ 0009.回文数/0009-回文数.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // 特殊情况：
+        // 如上所述，当 x < 0 时，x 不是回文数。
+        // 同样地，如果数字的最后一位是 0，为了使该数字为回文，只有0
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        //如此一来，保证“只翻转一半长度”，翻转一半结果revertedNumber最多比x多一位
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        // 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
+        // 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
+        // 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+};
