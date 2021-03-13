ba62adbd803db60f6b85273d6168642d535db60a
Huqingnan123 <897703334@qq.com>
2021-03-13 19:31:53 +0800

    2021-03-13

--- 0031.下一个排列/0031-下一个排列.cpp
+++ 0031.下一个排列/0031-下一个排列.cpp
@@ -5,19 +5,16 @@ public:
         //寻找第一个非逆序排列的元素
         for(index = nums.size() - 1; index > 0; index--)
         {
-            if(nums[index-1] >= nums[index])
-                continue;
-            else
+            if(nums[index-1] < nums[index])
                 break;
         }
         //当不存在下一个更大的排列时，重新排列成最小的排列（升序排列）,全部逆序
         if(index == 0)
-            reverse(nums.begin(),nums.end());
+            reverse(nums.begin(), nums.end());
         //index - 1对应的元素即为第一个非逆序元素，与最后一个大于它的元素互换位置，再reverse之后序列
         else
         {
             int last = nums.size()-1;
-            int temp_value;
             while(nums[last] <= nums[index - 1])
                 last--;
             swap(nums[last],nums[index - 1]);
